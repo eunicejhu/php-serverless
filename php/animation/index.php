9c25ce43161799ca6b74d205cebe3b2ab68060d7
ZUOQIN HU (Isabella) <eunicejhu@gmail.com>
2022-06-20 21:32:33 +0200

    feat: add animation

---
+++ php/animation/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animation</title>
+    <link rel="icon" href="./favicon.ico">
+    </link>
+    <link rel="stylesheet" href="./style.css">
+    </link>
+
+</head>
+
+<body>
+    <div class="container">
+
+
+
+
+        <div id="background-wrap">
+
+            <div class="image x1">
+                <img style="width:200px;height:200px;" src="./images/cocktail.png">
+            </div>
+
+            <div class="heart x3"></div>
+            <div class="heart x4"></div>
+            <div class="heart x5"></div>
+            <div class="heart x6"></div>
+            <div class="heart x7"></div>
+            <div class="heart x8"></div>
+            <div class="heart x9"></div>
+            <div class="heart x10"></div>
+
+
+            <div class="image x14">
+                <img src="./images/icons8-apple-logo-500.png">
+            </div>
+            <div class="image x7">
+                <img width="300" height="300" src="./images/icons8-nike-500.png">
+            </div>
+            <div class="image x12">
+                <img width="400" height="400" src="./images/icons8-amazon-500.png">
+            </div>
+            <div class="image x2">
+                <img width="200" height="200" src="./images/icons8-tick-box-256.png">
+            </div>
+            <div class="image x10">
+                <img width="200" height="200" src="./images/icons8-chrome-500.png">
+            </div>
+
+            <div class="image x12">
+                <img width="200" height="200" src="./images/icons8-health-500.png">
+            </div>
+            <div class="image x13">
+                <img width="200" height="200" src="./images/icons8-lipstick-100.png">
+            </div>
+            <div class="image x14">
+                <img width="200" height="200" src="./images/icons8-reminders-500.png">
+            </div>
+            <div class="image x15">
+                <img width="300" height="300" src="./images/icons8-starbucks-500.png">
+            </div>
+            <div class="heart x3"></div>
+            <div class="heart x4"></div>
+            <div class="heart x5"></div>
+            <div class="heart x6"></div>
+            <div class="heart x7"></div>
+            <div class="heart x8"></div>
+            <div class="heart x9"></div>
+            <div class="heart x10"></div>
+
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ php/typewriter/typewriter.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Typewriter</title>
+    <link rel="stylesheet" href="./styles.css">
+    </link>
+</head>
+
+<body>
+    <div>
+        <h1> Hello! My name is HU ZUOQIN. </h1>
+    </div>
+</body>
+
+</html>
